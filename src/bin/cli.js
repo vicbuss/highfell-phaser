e6b5cb8ac749b828ee11c1e7b089529c855e6d2a
Victor Bussiki <victorbussiki@gmail.com>
2023-03-22 20:06:37 -0300

    First CLI setup

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -1,8 +1,56 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs');
 const {moveHighFell} = require('../utilities/functions');
 const {highFellPositions} = require('../utilities/constants'); 
 
-console.log("Start");
+yargs
+//   .scriptName("highfell-mover")
+  .usage('$0 <cmd> [args]')
+  .command('move', ': moves Highfell for a number of periods (default 1)', (yargs) => {
+    yargs.positional('period', {
+      type: 'int',
+      default: 1,
+      describe: 'the number of periods to move Highfell for'
+    })
+  }, function (argv) {
+    moveHighFell(argv.period);
+    console.log(highFellPositions);
+  })
+  .help()
+  .example('$0 move --period [number]', 'moves Highfell for [number] periods')
+  .argv
 
-moveHighFell(1);
 
-console.log(highFellPositions);
+
+
+// var yargs = require('yargs/yargs')(process.argv.slice(2)).argv;
+// const {moveHighFell} = require('../utilities/functions');
+// const {highFellPositions} = require('../utilities/constants'); 
+
+
+// yargs.usage("\nUsage: $0 [cmd] <args>").alias("h", "help");
+
+// argv
+//     .command(
+//         'move',
+//         'Move Highfell by a certain number of periods (default 1)',
+//         {
+//             periods : {
+//                 type: 'int',
+//                 describe: 'The number of periods Highfell will be moved - (default 1)'
+//             }
+//         },
+//         function(argv) {
+//             moveHighFell(argv);
+//             console.log(highFellPositions);
+//         }
+//     )
+//     .help()
+//     .argv
+
+// // console.log("Start");
+
+// // moveHighFell(1);
+
+// // console.log(highFellPositions);
